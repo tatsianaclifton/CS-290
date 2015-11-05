a24709c1918fe650f92a0b6fb3733479c2462df7
Tatsiana Clifton <tatsianaclifton@gmail.com>
2015-11-04 20:48:34 -0500

    Get part is ready

--- Ajax-Interactions/script.js
+++ Ajax-Interactions/script.js
@@ -7,15 +7,22 @@ function bindButtons(){
     document.getElementById('zipSubmit').addEventListener('click', function(event){
         var req = new XMLHttpRequest();
         var zip = document.getElementById('zip').value;
-        req.open('GET', "http://api.openweathermap.org/data/2.5/weather?zip="+zip+",us&appid="+ apiKey, true);
+        var city = document.getElementById('city').value;
+        if (city != ""){
+            req.open('GET', "http://api.openweathermap.org/data/2.5/weather?q="+city+",us&appid="+ apiKey+"&units=imperial", true);
+        }
+        else{
+            req.open('GET', "http://api.openweathermap.org/data/2.5/weather?zip="+zip+",us&appid="+ apiKey+"&units=imperial", true);
+        }
         req.addEventListener('load', function(){
             if(req.status>=200 && req.status<400){
                 var response = JSON.parse(req.responseText);
+                document.getElementById("title").textContent = "Weather in you city";
                 document.getElementById("cityName").textContent = "City name: " + response.name;
-                document.getElementById("temp").textContent = "Temperature, F: " + (Math.trunc(response.main.temp)*9 / 5 - 459.67);
-                document.getElementById("humidity").textContent = "Humidity, %: " + response.main.humidity;
-                document.getElementById("cloud").textContent = "Clouds, %: " + response.clouds.all;
-                document.getElementById("wind").textContent = "Wind, mph: " + response.wind.speed;
+                document.getElementById("temp").textContent = "Temperature: " + response.main.temp + " F";
+                document.getElementById("humidity").textContent = "Humidity: " + response.main.humidity + " %";
+                document.getElementById("cloud").textContent = "Clouds: " + response.clouds.all + " %";
+                document.getElementById("wind").textContent = "Wind: " + response.wind.speed + " mph";
             }
             else{
                 console.log("error in network request: " + request.statusText);
