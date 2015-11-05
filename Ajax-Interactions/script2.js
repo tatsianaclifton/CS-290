efa9a8f3470121fb337180404f327d2175e3f48d
Tatsiana Clifton <tatsianaclifton@gmail.com>
2015-11-05 10:16:23 -0500

    Do not know how to display JSON in HTML

--- Ajax-Interactions/script2.js
+++ Ajax-Interactions/script2.js
@@ -5,14 +5,15 @@ document.addEventListener('DOMContentLoaded', bindButtons);
 function bindButtons(){
     document.getElementById('submit').addEventListener('click', function(event){
         var req = new XMLHttpRequest();
-        var payload = {str:null};
+        var payload = {str: null};
         payload.str = document.getElementById('string').value;
         req.open('POST', "http://httpbin.org/post", true);
         req.setRequestHeader('Content-Type', 'application/json');
         req.addEventListener('load', function(){
             if(req.status>=200 && req.status<400){
                 var response = JSON.parse(req.responseText);
-                document.getElementById('data').textContent = "You entered: " + response;
+                console.log(response);
+                document.getElementById('data').textContent = "You entered: " + response.data;
             }
             else{
                 console.log("error in network request: " + req.statusText);
